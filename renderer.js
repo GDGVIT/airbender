63b3a0b07a3076062ac930c61afb9120f166604d
Ananya Ganesh <silver@pop-os.localdomain>
2020-09-03 16:50:59 +0530

    Dynamic listing of members

--- renderer.js
+++ renderer.js
@@ -1,10 +1,30 @@
 // In renderer process (web page).
 const { ipcRenderer } = require('electron')
 
+let memberList;
 ipcRenderer.on('speakerInfo', (event, message) => {
   console.log("From Main Speaker list",message)
 })
 
 ipcRenderer.on('membersInfo', (event, message) => {
   console.log("From Main Members list",message)
+  memberList = message;
+  members();
 })
+
+function members(){
+  let members_list = '';
+  for (var it=memberList[0].values(), val = null; val=it.next().value; ) {
+    console.log(val);
+    members_list += `
+    <div class="person">
+      <img src="./assets/blushing_dino.png" class="icon">
+      <div class="info">
+        <p>${val.username}</p>
+        <!-- <p>Head of Dinos</p> -->
+      </div>
+    </div>
+    `
+  } 
+  document.getElementById('container').innerHTML = members_list;
+}

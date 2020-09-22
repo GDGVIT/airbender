c58b90549ea8a8d0d867cfe0ca5bd13812016454
SaurusXI <vermashantanu@hotmail.com>
2020-09-22 12:28:14 +0530

    add speechchange example in index.js

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const { memberList } = require('./bot');
 console.log("Users currently in channel: ", memberList);
 
 // To get speech change info, listen to this event:
-bot.on('speechChange', currentState => {
-    console.log(currentState);
+bot.on('speechChange', ([user, speaking]) => {
+    console.log(user, speaking);
 });
 
 // To get info on users in channel, listen to this event (updates memberList whenever users enter/leave channel)
 bot.on('voiceStateUpdate', memberList => {
     console.log("Channel members: ", memberList);
+    
+    for (var it=memberList[0].values(), val = null; val=it.next().value; ) {
+        console.log(val);
+    } 
 })
